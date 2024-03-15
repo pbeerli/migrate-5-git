7f21d6829b5d31b6470593b29826ae616861966a
Peter Beerli <beerli@mac.com>
2024-03-15 14:42:10 -0400

    putting migrate version 5.0 into github

---
+++ src/src/pretty-win32.cpp
@@ -0,0 +1,29 @@
+///////////////
+// on windows systems this file is need when compiling migrate
+// using the libharu PDF-package.
+//
+// Michal Palzcewski and Peter Beerli 2005
+#ifdef WINDOWS
+#include <new.h>
+#ifndef MPI
+extern "C" {
+  void set_haru_handler(void);
+}
+//extern int throw_new_handler(size_t t);
+std::new_handler throw_new_handler;
+void set_haru_handler(void) 
+{
+  set_new_handler(throw_new_handler);
+}
+#endif
+#endif
+int dummy_function(int do_nothing);
+
+int dummy_function(int do_nothing)
+{
+  int x;
+  int y = 1.0;
+  //Some computer fail to compile MIGRATE with empty files
+  x = do_nothing + y;
+  return x;
+}
